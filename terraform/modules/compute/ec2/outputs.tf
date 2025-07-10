4c24af648c73ce91440fb8e1da7b3dff3f67d389
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-07-11 01:28:28 +0900

    feat: ec2 모듈 생성

---
+++ terraform/modules/compute/ec2/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_id" {
+  description = "생성된 EC2 인스턴스의 ID"
+  value       = aws_instance.main.id
+}
+
+output "public_ip" {
+  description = "EC2 인스턴스의 퍼블릭 IP (있는 경우)"
+  value       = aws_instance.main.public_ip
+}
+
+output "private_ip" {
+  description = "EC2 인스턴스의 프라이빗 IP"
+  value       = aws_instance.main.private_ip
+}
+
+output "availability_zone" {
+  description = "EC2 인스턴스가 생성된 가용 영역"
+  value       = aws_instance.main.availability_zone
+}
