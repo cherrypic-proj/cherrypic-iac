ceeb62298547cabfd59f2fe2dc64b64c8a901574
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-07-10 23:11:17 +0900

    Revert "feat: network 모듈 구현 (VPC, Subnet , Route Table , IGW , Elastic IP)"

--- terraform/modules/network/elasticip/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "EIP의 ID"
-  value       = aws_eip.main.id
-}
-
-output "public_ip" {
-  description = "EIP의 퍼블릭 IP"
-  value       = aws_eip.main.public_ip
-}
--- terraform/modules/network/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id
-}
-
-output "vpc_tags" {
-  description = "적용된 VPC 태그"
-  value       = aws_vpc.main.tags
-}
