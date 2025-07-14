d4bf48e5715fb306b3d152a4a64534bc849c6bdb
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-07-14 20:00:50 +0900

    fix: bastion host 명명 규칙 수정 및 ami 변경

--- terraform/env/dev/compute.tf
+++ terraform/env/dev/compute.tf
@@ -1,7 +1,7 @@
 module "bastion_dev" {
   source = "../../modules/compute/ec2"
 
-  ami_id                 ="ami-06af2834df25fe45d" // Bastion backup AMI
+  ami_id                 = "ami-06af2834df25fe45d" // Bastion backup AMI
   instance_type          = "t2.micro"
   subnet_id              = module.public_subnet_1.id
   vpc_security_group_ids = [module.bastion_dev_sg.id]
