a9dc19a87cd303a54663cf4466911c492ad609d3
현태 최 <ht3064@naver.com>
2025-08-19 17:40:30 +0900

    chore: dev 환경에서 Bastion Host 관련 리소스 제거

--- terraform/env/dev/compute.tf
+++ terraform/env/dev/compute.tf
@@ -1,21 +1,3 @@
-module "bastion_dev" {
-  source = "../../modules/compute/ec2"
-
-  ami_id                 = "ami-06af2834df25fe45d" // Bastion backup AMI
-  instance_type          = "t2.micro"
-  subnet_id              = module.public_subnet_1.id
-  vpc_security_group_ids = [module.bastion_dev_sg.id]
-  associate_public_ip    = false
-  key_name               = "cherrypic-bastion-host-key"
-
-  root_volume_size = 30
-  root_volume_type = "gp3"
-  purpose          = "bastion-host"
-  environment      = ""
-  enable_eip       = false
-  tags             = local.common_tags
-}
-
 module "api_dev" {
   source = "../../modules/compute/ec2"
 
--- terraform/env/dev/network.tf
+++ terraform/env/dev/network.tf
@@ -92,36 +92,6 @@ module "api_dev_sg" {
   tags = local.common_tags
 }
 
-module "bastion_dev_sg" {
-  source      = "../../modules/network/securitygroup"
-  purpose     = "bastion-host"
-  env         = ""
-  description = "Bastion Host Security Group"
-  vpc_id      = module.vpc.id
-
-  ingress_rules = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      use_cidr    = true
-      use_sg      = false
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-
-  egress_rules = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-
-  tags = local.common_tags
-}
-
 module "cache_dev_sg" {
   source      = "../../modules/network/securitygroup"
   purpose     = "cache"
@@ -137,15 +107,7 @@ module "cache_dev_sg" {
       use_cidr                 = false
       use_sg                   = true
       source_security_group_id = module.api_dev_sg.id
-    },
-    {
-      from_port                = 6379
-      to_port                  = 6379
-      protocol                 = "tcp"
-      use_cidr                 = false
-      use_sg                   = true
-      source_security_group_id = module.bastion_dev_sg.id
-    },
+    }
   ]
 
   egress_rules = [
@@ -175,14 +137,6 @@ module "db_dev_sg" {
       use_cidr                 = false
       use_sg                   = true
       source_security_group_id = module.api_dev_sg.id
-    },
-    {
-      from_port                = 3306
-      to_port                  = 3306
-      protocol                 = "tcp"
-      use_cidr                 = false
-      use_sg                   = true
-      source_security_group_id = module.bastion_dev_sg.id
     }
   ]
 
